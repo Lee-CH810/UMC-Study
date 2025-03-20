7e03ab531eb4697f41ea30f91dcf7bbc840591bf
Leechanghwan <2002ckdghks99@gmail.com>
2025-03-20 12:49:38 +0900

    (번외) lv4에 뒤로 버튼 만들었으나 작동X

--- app/src/main/java/com/example/umcstudy/lv4Activity.kt
+++ app/src/main/java/com/example/umcstudy/lv4Activity.kt
@@ -1,14 +1,29 @@
 package com.example.umcstudy
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.umcstudy.databinding.ActivityLv4Binding
 
 class lv4Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /**
+         * 화면 출력*/
+        // 바인딩 객체 획득
+        val b4 = ActivityLv4Binding.inflate(layoutInflater)
+        // 화면에 콘텐츠: b4 객체의 루트 요소 출력
+        setContentView(b4.root)
+
+        /**
+         * 버튼 이벤트 처리: 다시 뒤로 돌아가기
+         */
+        b4.back4.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+        }
+
+        /*
         enableEdgeToEdge()
         setContentView(R.layout.activity_lv4)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +31,7 @@ class lv4Activity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+         */
     }
 }
